106253ade23fa0212e7403453d85f0007350586d
peterkwesiansah <ansahpeter123@yahoo.com>
2026-06-20 19:59:03 +0000

    worker handles map/reduce tasks scheduled by coordinator

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -6,10 +6,11 @@ import (
 	"hash/fnv"
 	"io"
 	"log"
+	"net"
+	"net/http"
 	"net/rpc"
 	"os"
 	"sort"
-	"sync"
 )
 
 // for sorting by key.
@@ -19,26 +20,6 @@ type ByKey []KeyValue
 func (a ByKey) Len() int           { return len(a) }
 func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
-
-var activeMaxMapTasks int = 8
-var failedConns int = 0
-
-type taskState int
-
-// 2. Use iota to auto-increment values
-const (
-	active taskState = iota
-	idle
-)
-
-type workerTask struct {
-	state   taskState
-	payload Task
-}
-
-type workerTasks struct {
-	tasks []workerTask
-}
 
 // Map functions return a slice of KeyValue.
 type KeyValue struct {
@@ -55,192 +36,231 @@ func ihash(key string) int {
 }
 
 var coordSockName string // socket for coordinator
+var workerSockName string
+var waitTilDone chan bool
+
+type MapCounts struct {
+	Partitions int
+}
+
+// MapState
+type MapState struct {
+	//M piece
+	task           Task
+	mapReduceTasks []string
+	counts         MapCounts
+}
+
+type ReducerCounts struct {
+	State workerState
+	//number of unique intermediate Keys
+	Keys int
+	// total number of intermediate Keys
+	Values int
+}
+
+type ReducerState struct {
+	//R partition
+	intermediate []KeyValue
+	reduceId     int
+	counts       ReducerCounts
+}
+
+func (ms *MapState) Snapshot(taskId TaskId, reply *MapCounts) error {
+	if taskId != TaskId(ms.task.TaskId) {
+		return fmt.Errorf("Expected %d taskId but got %d. Check state on master.", ms.task.TaskId, taskId)
+	}
+	*reply = MapCounts{Partitions: ms.counts.Partitions}
+	return nil
+}
+
+// start a thread that listens for RPCs from worker.go
+func (ms *MapState) server(sockname string) {
+	rpc.Register(ms)
+	rpc.HandleHTTP()
+	os.Remove(sockname)
+	l, e := net.Listen("unix", sockname)
+	if e != nil {
+		log.Fatalf("listen error %s: %v", sockname, e)
+	}
+	fmt.Printf("Worker listening at %s\n", sockname)
+	go http.Serve(l, nil)
+}
+
+func (rs *ReducerState) Snapshot(reduceId int, reply *ReducerCounts) error {
+	if reduceId != rs.reduceId {
+		return fmt.Errorf("Expected %d reduceId but got %d. Check state on master.", rs.reduceId, reduceId)
+	}
+	*reply = rs.counts
+	if rs.counts.State == completed {
+		waitTilDone <- true
+	}
+	return nil
+}
+
+// start a thread that listens for RPCs from worker.go
+func (rs *ReducerState) server(sockname string) {
+	rpc.Register(rs)
+	rpc.HandleHTTP()
+	os.Remove(sockname)
+	l, e := net.Listen("unix", sockname)
+	if e != nil {
+		log.Fatalf("listen error %s: %v", sockname, e)
+	}
+	fmt.Printf("Worker listening at %s\n", sockname)
+	go http.Serve(l, nil)
+}
 
 // main/mrworker.go calls this function.
 // A reasonable naming convention for intermediate files is mr-X-Y, where X is the Map task number, and Y is the reduce task number.
 func Worker(sockname string, mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
-
+	waitTilDone = make(chan bool)
 	coordSockName = sockname
+	workerSockName = coordinatorSock()
+	phase := os.Getenv("PHASE")
 
-	var tasks workerTasks = workerTasks{}
-	var mu sync.Mutex
-
-	notifyReducerCh := make(chan struct {
-		mapTask TaskId
-		output  []string
-	})
-
-	var done bool = false
-	// This go routine will check the status of the job
-	go func(status *bool) {
-		connRetries := 0
-		rpcRetries := 0
-		for {
-			if *status {
-				break
-			}
-			if connRetries == 5 {
-				*status = true
-			}
-			c, err := connectrpc()
-			if err != nil {
-				fmt.Printf(fmt.Errorf("Client Connection Failed for Job Status Check: %v\n", err).Error())
-				connRetries++
-				// How many retries before we give up
-				continue
-			}
-			err = callrpc(c, "Coordinator.JobStatus", 0, status)
-			if err != nil {
-				fmt.Printf(fmt.Errorf("RPC Failed: %v\n", err).Error())
-				// How many retries before we give up
-				rpcRetries++
-				c.Close()
-				continue
-			}
-		}
-	}(&done)
-	//waiting for finished mapTasks
-	// when do the worker process return ??
-	go func() {
-		// reducers
-		for {
-			reduceNotif := <-notifyReducerCh
-			go func() {
-				defer func() {
-					// how do we recover from crashes??
-				}()
-				for _, reducerTask := range reduceNotif.output {
-					var reduceKey int
-					var task int
-					intermediate := []KeyValue{}
-					fmt.Sscanf(reducerTask, "mr-%d-%d", &task, &reduceKey)
-					//assert task==reduceNotif.task
-					if task != int(reduceNotif.mapTask) {
-						log.Fatal("ReduceKeys Mismatch")
-					}
-					file, err := os.Open(reducerTask)
-					if err != nil {
-						log.Fatal(err)
-					}
-					dec := json.NewDecoder(file)
-					for {
-						var kv KeyValue
-						if err := dec.Decode(&kv); err != nil {
-							break
-						}
-						intermediate = append(intermediate, kv)
-					}
-					file.Close()
-					i := 0
-					for i < len(intermediate) {
-						j := i + 1 // number of key count ?
-						//compares the current and subsequent keys and increment j if they are the same
-						for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
-							j++
-						}
-						values := []string{}
-						//saves them in value
-						for k := i; k < j; k++ {
-							values = append(values, intermediate[k].Value)
-						}
-						output := reducef(intermediate[i].Key, values)
-						writeTo := fmt.Sprintf("mr-out-%d", reduceKey)
-						mu.Lock()
-						ofile, err := os.OpenFile(writeTo, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-						if err != nil {
-							log.Fatal(err)
-						}
-						fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
-						mu.Unlock()
-						ofile.Close()
-						// this is the correct format for each line of Reduce output.
-						i = j
-					}
-				}
-			}()
-		}
-	}()
-
-	connRetriesMapTasks := 0
-	// Worker
-	for {
-		if done {
-			//till we figure when the worker process exits, as the mapTasks , reduceTasks lifecyle are tied together
-			for {
-			}
-		}
-		if connRetriesMapTasks == 5 {
-			for {
-			}
-		}
-		c, err := connectrpc()
+	if len(phase) == 0 || phase == "0" {
+		fmt.Printf("Starting worker in map phase with PID %d\n", os.Getpid())
+		c, err := connectrpc(coordSockName)
 		if err != nil {
 			fmt.Printf(fmt.Errorf("Client Connection Failed: %v\n", err).Error())
-			connRetriesMapTasks++
-			// How many retries before we give up
-			continue
+			return
 		}
-		connRetriesMapTasks = 0
-		var path Task
-		taskId := len(tasks.tasks)
-		err = callrpc(c, "Coordinator.GetTask", taskId, &path)
+		ms := MapState{}
+
+		err = callrpc(c, "Coordinator.GetmTask", WorkerInfo{
+			WorkerId: os.Getpid(),
+			Type:     0,
+			Sockname: workerSockName,
+		}, &ms.task)
+		defer c.Close()
+		if err != nil {
+			fmt.Printf(fmt.Errorf("Worker Id %d: %v\n", ms.task.TaskId, err).Error())
+			return
+		}
+		// for heartbeat messages between coordinator and worker
+		go func() { ms.server(workerSockName) }()
+		//do work with Task
+		file, err := os.Open(ms.task.Path)
+		if err != nil {
+			log.Fatalf("cannot open %v", ms.task.Path)
+		}
+		content, err := io.ReadAll(file)
+		if err != nil {
+			log.Fatalf("cannot read %v", ms.task.Path)
+		}
+		file.Close()
+		kva := mapf(ms.task.Path, string(content))
+
+		fps := make([]*os.File, 0, ms.task.ReduceTasks)
+		ms.mapReduceTasks = make([]string, 0, ms.task.ReduceTasks)
+
+		for partition := range ms.task.ReduceTasks {
+			writeTo := fmt.Sprintf("mr-%d-%d", ms.task.TaskId, partition)
+			//open file ms.task.ReduceTasks times
+			fp, err := os.OpenFile(writeTo, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+			if err != nil {
+				log.Fatalf("cannot open %v", writeTo)
+			}
+			ms.mapReduceTasks = append(ms.mapReduceTasks, writeTo)
+			fps = append(fps, fp)
+		}
+
+		//TODO: use a combiner function --use a flag and compare memory usage and performance metrics with or without it
+		// TODO: map[partition]count
+		for _, kv := range kva {
+			partition := ihash(kv.Key) % ms.task.ReduceTasks
+			writeTo := ms.mapReduceTasks[partition]
+			fp := fps[partition]
+			enc := json.NewEncoder(fp)
+			err = enc.Encode(&kv)
+			if err != nil {
+				log.Fatalf("cannot write to buffered Key/Value Pair to %v", writeTo)
+			}
+		}
+
+		reply := -1
+		err = callrpc(c, "Coordinator.Notify", NotifyTask{
+			MapTask:     ms.task,
+			WorkerId:    WorkerId(os.Getpid()),
+			ReduceTasks: ms.mapReduceTasks,
+		}, &reply)
 
 		if err != nil {
-			c.Close()
-			fmt.Printf(fmt.Errorf("Task Id %d: %v\n", taskId, err).Error())
-			connRetriesMapTasks++
-			continue
-			// How many retries before we give up
+			fmt.Printf(fmt.Errorf("Worker Id %d: %v\n", ms.task.TaskId, err).Error())
+			return
 		}
-		fmt.Printf("Spawning Map Worker Task %d\n", taskId)
-		tasks.tasks = append(tasks.tasks, workerTask{
-			payload: path,
-			state:   active,
-		})
-		go func(taskId TaskId, path Task) {
-			defer func() {
-				//c.Close()
-			}()
-			//do work with Task
-			file, err := os.Open(path.Path)
+
+	} else {
+		fmt.Printf("Starting worker in reduce phase with PID %d\n", os.Getpid())
+		c, err := connectrpc(coordSockName)
+		if err != nil {
+			fmt.Printf(fmt.Errorf("Client Connection Failed: %v\n", err).Error())
+			return
+		}
+
+		var task RTask
+
+		err = callrpc(c, "Coordinator.GetrTask", WorkerInfo{
+			WorkerId: os.Getpid(),
+			Type:     1,
+			Sockname: workerSockName,
+		}, &task)
+		c.Close()
+		if err != nil {
+			fmt.Printf(fmt.Errorf("Worker Id %d: %v\n", task.ReduceId, err).Error())
+			return
+		}
+		rs := ReducerState{intermediate: []KeyValue{}}
+		// for heartbeat messages between coordinator and worker
+		go func() { rs.server(workerSockName) }()
+		//per mapTask
+		for _, reducerTask := range task.MapOutputs {
+			file, err := os.Open(reducerTask)
 			if err != nil {
-				log.Fatalf("cannot open %v", path.Path)
+				log.Fatal(err)
 			}
-			content, err := io.ReadAll(file)
-			if err != nil {
-				log.Fatalf("cannot read %v", path.Path)
+			dec := json.NewDecoder(file)
+			for {
+				var kv KeyValue
+				if err := dec.Decode(&kv); err != nil {
+					break
+				}
+				rs.intermediate = append(rs.intermediate, kv)
 			}
 			file.Close()
-			kva := mapf(path.Path, string(content))
-			sort.Sort(ByKey(kva))
-			mapReduceTasks := []string{}
-			for _, kv := range kva {
-				writeTo := fmt.Sprintf("mr-%d-%d", taskId, ihash(kv.Key)%path.ReduceTasks)
-				//create reduceTasks
-				reduceTaskFile, err := os.OpenFile(writeTo, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-				if err != nil {
-					log.Fatalf("cannot open %v", writeTo)
-				}
-				enc := json.NewEncoder(reduceTaskFile)
-				err = enc.Encode(&kv)
-				if err != nil {
-					log.Fatalf("cannot write to buffered Key/Value Pair to %v", writeTo)
-				}
-				mapReduceTasks = append(mapReduceTasks, writeTo)
-				reduceTaskFile.Close()
+		}
+		sort.Sort(ByKey(rs.intermediate))
+		// combiner code
+		i := 0
+		writeTo := fmt.Sprintf("mr-out-%d", task.ReduceId)
+		ofile, err := os.OpenFile(writeTo, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		for i < len(rs.intermediate) {
+			j := i + 1 // number of key count ?
+			//compares the current and subsequent keys and increment j if they are the same
+			for j < len(rs.intermediate) && rs.intermediate[j].Key == rs.intermediate[i].Key {
+				j++
 			}
-			notifyReducerCh <- struct {
-				mapTask TaskId
-				output  []string
-			}{
-				mapTask: taskId,
-				output:  mapReduceTasks,
+			values := []string{}
+			//saves them in value
+			for k := i; k < j; k++ {
+				values = append(values, rs.intermediate[k].Value)
 			}
-		}(TaskId(taskId), path)
-		c.Close()
+			output := reducef(rs.intermediate[i].Key, values)
+			rs.counts.Keys++
+			rs.counts.Values += len(values)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Fprintf(ofile, "%v %v\n", rs.intermediate[i].Key, output)
+			// this is the correct format for each line of Reduce output.
+			i = j
+		}
+		rs.counts.State = completed
+		ofile.Close()
+		<-waitTilDone
 	}
-
 }
 
 // send an RPC request to the coordinator, wait for the response.
@@ -250,12 +270,11 @@ func callrpc(c *rpc.Client, rpcname string, arg any, reply any) error {
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
-func connectrpc() (*rpc.Client, error) {
-	c, err := rpc.DialHTTP("unix", coordSockName)
+func connectrpc(sockName string) (*rpc.Client, error) {
+	c, err := rpc.DialHTTP("unix", sockName)
 	if err != nil {
 		return nil, err
 	}
